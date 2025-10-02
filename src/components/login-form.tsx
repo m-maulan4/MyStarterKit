289fbbbcc2df5d095d843dc1fb4d8b3a9115f23f
Muammar Maulana <muammar676@gmail.com>
2025-10-02 10:46:14 +0700

    add page login

---
+++ src/components/login-form.tsx
@@ -0,0 +1,66 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { useAuth } from "@/hooks/authLogin";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"form">) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, isLoading, loginError } = useAuth();
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      await login(username, password);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Enter your Username below to login to your account
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-3">
+            <Label htmlFor="username">Username</Label>
+            <Input
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          {loginError && (
+            <div className="error-message">
+              Login failed. Please check your credentials.
+            </div>
+          )}
+          <Button type="submit" className="w-full" disabled={isLoading}>
+            {isLoading ? "Logging in..." : "Login"}
+          </Button>
+        </form>
+      </div>
+    </form>
+  );
+}
